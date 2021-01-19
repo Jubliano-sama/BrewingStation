12ddb035bbf5fa95513a016c0a9e9a4c16322061
BamiHapje <77484817+BamiHapje@users.noreply.github.com>
2021-01-19 15:05:23 +0100

    added /addMix command

--- BookController.py
+++ BookController.py
@@ -21,6 +21,7 @@ def listMixenForPrint():
                 
         return msg
 
+#returnt een lijst van flessen
 def listFlessen():
         with open('flessen.json') as json_file:
                 data = json.load(json_file)
@@ -35,72 +36,19 @@ def listFlessenForPrint():
                 
         return msg
 
-def addMix():
-        newDrinkName = input("Naam van het nieuwe drankje: ")
 
-        #loop om de alleen door te gaan met 0 < int < 14
-        numberOfIngredients = -1
-        while numberOfIngredients <= 0 or numberOfIngredients > 14:
-                numberOfIngredientsInput = input("Aantal bestandsdelen: ")
-                try:
-                        numberOfIngredients = int(numberOfIngredientsInput)
-                except: 
-                        print("De opgegeven waarde moet een geheel getal zijn van 1 t.e.m. 14 zonder spaties,\n of geef het commando 'CANCEL'") 
-
-                if numberOfIngredients > 14:
-                        print("Er kunnen niet meer als 14 verschillende dranken worden gekozen.")
-
-        # de ingredienten naar verhouding toevoegen
-        ingredients = {}
-        with open("flessen.json") as data:
-                baverages = json.load(data)
-        for i in range(numberOfIngredients):
-                while True:
-                        message = "ingredient #" + str(i + 1) + ": "
-                        ingredient = str.lower(input(message))
-                        if ingredient in baverages: 
-                                break
-                        else:
-                                command = input("dit drankje bestaat nog niet in de database wil u het toevoegen of in de lijst kijken? (z/k)")
-                                #hier was je 
-                message = ingredient + " maakt zoveel delen van " + newDrinkName + ": "
-                parts = str.lower(input(message))                  
-                ingredients[ingredient] = int(parts)
-
-        #recept opbouwen
+def addMix(newDrinkName, ingredients):
         page = {
         "name" : newDrinkName,
         "composition": ingredients
         }
 
-        #controle 
-        print("ter controle:")
-        print("Naam nieuwe drankje:", newDrinkName)
-        print("Met ingredienten: ")
-        for fluid in sorted(ingredients):
-                print(fluid, ingredients[fluid], "delen")
-
-        while True:
-                lastCheck = input("Goedgekeurd? (j/n)")
-
-                if lastCheck == "j":
-
-                        #with open("Receptboek.json") as data:
-                        with open('Receptboek.json') as json_file:
-                                data = json.load(json_file)
-                                data.append(page)
-                        outfile = open("Receptboek.json", "w+")
-                        json.dump(data, outfile, indent = 4)
-                        outfile.close()
-                                #data.close()
-
-                        print(newDrinkName + "is toegevoed aan het systeem")
-                        break   
-                elif lastCheck == "n":
-                        print("jammer, probeer het opnieuw.")
-                        break
-                else:
-                        print("invalide input, probeer opnieuw.")
+        with open('Receptboek.json') as json_file:
+                data = json.load(json_file)
+                data.append(page)
+        outfile = open("Receptboek.json", "w+")
+        json.dump(data, outfile, indent = 4)
+        outfile.close()
 
 def addFles(name):
         data = listFlessen()
--- telegramBot.py
+++ telegramBot.py
@@ -7,9 +7,15 @@ bot = telebot.TeleBot(token)
 
 #https://github.com/eternnoir/pyTelegramBotAPI/blob/master/examples/step_example.py
 
-ValidCommands = ['addFles', 'listFles', 'listMix', 'help', 'removeFles']
-#keyboard = telebot.types.ReplyKeyboardMarkup(True)
-#keyboard.row('/addFles', '/listFles', '/listMix', '/help', '/removeFles')
+ValidCommands = ['addFles', 'listFles', 'listMix', 'help', 'removeFles', 'addMix']
+
+#global variabel
+
+#voor addMix
+composition = {}
+mixName = ""
+
+
 
 @bot.message_handler(commands=ValidCommands)
 def mainHandler(message):
@@ -33,11 +39,14 @@ def mainHandler(message):
 			msg += '/' + command + '\n'
 		
 		bot.send_message(message.chat.id, msg)
-		
 	
 	elif boodschap == "/removeFles":
 		msg = bot.send_message(message.chat.id, "Aaahhhh een drankje verwijderen, komt goed! wat is de naam?")
 		bot.register_next_step_handler(msg, handleFlesNameRemove)
+	
+	elif boodschap == "/addMix":
+		msg = bot.send_message(message.chat.id, "Wat is de naam van deze nieuwe smaak-sensatie")
+		bot.register_next_step_handler(msg, handleMixNameAdd)
 
 	else: 
 		bot.reply_to(message, "Onbekend commando gebruik '/help' voor een lijst met commando's")
@@ -52,6 +61,61 @@ def handleFlesNameRemove(message):
 def handleFlesNameAdd(message):
 	nameFlesToAdd = message.text
 	BookController.addFles(nameFlesToAdd)
+
+def handleMixNameAdd(message):
+	global mixName
+	mixName = message.text
+	returntext = "Oke we openen een nieuwe pagina in het kookboek schrijven voor een heerlijke Mixdrank. Je kunt zelf ingrdienten toevoegen dit doe je door een bericht te sturen in het volgende format\n [naamfles] [deel] \n b.v. \n 'Cola 20' \n\n Ben je klaar met het toevoegen van flessen? geef het commando 'KLAAR'"
+	msg = bot.send_message(message.chat.id, returntext)
+	bot.register_next_step_handler(msg, handleMixIngredients)
+
+def handleMixIngredients(message):
+	messageText = message.text
+	messageTextBackup = messageText
+	global composition 
+	global mixName
+	
+	if messageText != "KLAAR":
+
+		if messageText == "lijst":
+			msg = bot.send_message(message.chat.id, BookController.listFlessenForPrint())
+			bot.register_next_step_handler(msg, handleMixIngredients)
+
+		if messageText[0] == "+":
+			messageText = messageText[1:]
+			messageText = messageText.strip()
+			messageText = messageText.title()
+			messageList = messageText.split(" ")
+			naamFles = messageList[0]
+			BookController.addFles(naamFles)
+
+		try: 
+			messageText = messageText.strip()
+			messageText = messageText.title()
+			messageList = messageText.split(" ")
+			naamFles = messageList[0]
+			deelVanMix = int(messageList[1])
+			if naamFles in BookController.listFlessen():
+				composition[naamFles] = deelVanMix
+				msg = bot.send_message(message.chat.id, "Staat genoteerd. Voeg op de zelfde mannier uw volgende drankje toe, of om af te ronden stuur 'KLAAR'")
+				bot.register_next_step_handler(msg, handleMixIngredients)
+			else:
+				msg = bot.send_message(message.chat.id, "Deze fles staat niet in het systeem, herhaal je bericht met een een '+' (b.v. '+ Cola 20') of reageer met 'lijst' om een overzicht van beschikbarenflessen te zien")
+				bot.register_next_step_handler(msg, handleMixIngredients)
+		
+		except:
+			if messageText != "lijst":
+				msg = bot.send_message(message.chat.id, "Het format is incorrect probeer het opnieuw")
+				bot.register_next_step_handler(msg, handleMixIngredients)
+	
+	else: 
+		BookController.addMix(mixName, composition)
+		text = "Proficiat " + mixName + " staat nu in het receptenboek."
+		bot.send_message(message.chat.id, text)
+		
+
+
+
 
 #error handler
 @bot.message_handler(func=lambda message: True)
